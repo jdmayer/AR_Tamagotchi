effd199280bd255ef14514d6f38a4088006e2d2e
S2010745024 <79395966+S2010745024@users.noreply.github.com>
2021-03-05 14:28:08 +0100

    Skin Changer

---
+++ AR_Tamagotchi/Assets/Scripts/PanelOpener.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// Autor: Helena Wilde
+/// </summary>
+public class PanelOpener : MonoBehaviour
+{
+    public GameObject Panel;
+
+    public void OpenPanel()
+    {
+        if(Panel != null)
+        {
+            bool isActive = Panel.activeSelf;
+            Panel.SetActive(!isActive);
+                }
+    }
+}
