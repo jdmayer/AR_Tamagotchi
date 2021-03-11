9a9b235bea2ee737dd7c96f8d98b6ce4676224d0
jdmayer <32510229+jdmayer@users.noreply.github.com>
2021-03-11 12:35:53 +0100

    Add new markers and allocation script

---
+++ AR_Tamagotchi/Assets/Resources/MarkerAllocation.cs
@@ -0,0 +1,23 @@
+﻿/// <summary>
+/// This is no script but an overview about the allocation of the existing markers
+/// Meters (m) are the chosen size of units
+/// </summary>
+/// 
+/// stones - our Tamagotchi "Fino"
+/// chips
+/// tarmac
+/// marker-01
+/// marker-02
+/// marker-03
+/// marker-04
+/// marker-05
+/// marker-06
+/// marker-07
+/// marker-08
+/// marker-09
+/// marker-10
+/// marker-11
+/// marker-12
+/// marker-13
+/// marker-14
+/// marker-15
