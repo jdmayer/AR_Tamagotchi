a763f35989ac4e7bea241c7a9bebbe053b10a51e
jdmayer <32510229+jdmayer@users.noreply.github.com>
2021-04-05 17:13:02 +0200

    Add PlayerPref
    Contains constants for PlayerPrefs

---
+++ AR_Tamagotchi/Assets/Scripts/Utils/PlayerPref.cs
@@ -0,0 +1,18 @@
+﻿/// <summary>
+/// Author: Janine Mayer
+/// </summary>
+namespace Utils
+{
+    public static class PlayerPref
+    {
+        public const string Age = "Age";
+        public const string Name = "Name";
+        public const string Happy = "Happy";
+        public const string Energy = "Energy";
+        public const string Hunger = "Hunger";
+        public const string Social = "Social";
+
+        public const string ExperiencePoints = "ExperiencePoints";
+        public const string MaxHealth = "MaxHealth";
+    }
+}
